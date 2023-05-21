6019cc2e5512ab8c995c058a452dc27c7569c821
BurMinistr <papanoffk@ya.ru>
2023-05-22 00:56:42 +0300

    почти финалка, надо поправить косяки с отключением

--- window_chat.py
+++ window_chat.py
@@ -13,41 +13,62 @@ class Window_chat(CTk.CTk):
         self.grid_rowconfigure(0, weight=2)
         self.columnconfigure(2, weight=1)
 
+        self.window = None
         self.client = None
         self.chat_frame = None
 
-        #add entry for text message
-        self.entry = CTk.CTkEntry(self, placeholder_text="Entry message")
-        self.entry.grid(row=0, column=1, padx=20, pady=20)
+        #add label for important message
+        self.label = CTk.CTkLabel(self, text = 'Подключение')
+        self.label.grid(row=0, column=1)
 
-        #add button for input namme
-        def button_command():
-            _name = self.entry.get()
-            #проверка ввода имени
-            for ch in _name.split():
-                if ch != ' ':
-                    self.run_speak_with_server(_name)
-                    button.destroy()
-        button = CTk.CTkButton(self, text = 'Подключиться', command = button_command)
-        button.grid(row=0, column=2, padx=20, pady=20)
+        #add entry for text message
+        self.entry = CTk.CTkEntry(self, placeholder_text="Entry text")
+        self.entry.grid(row=1, column=1, padx=20, pady=20)
+
+        #create menu
+        self.screen_of_input_name()
 
         #close window
         def on_closing():
             print("Выводится при попытке закрытия окна")
             #проверка на существование объектов
             if self.chat_frame is not None:
-                 self.chat_frame.close()
+                 self.chat_frame.destroy()
             if self.client is not None:
                 self.client.__del__()
             self.destroy()  # Закрыть окно
         self.protocol("WM_DELETE_WINDOW", on_closing)
 
+    # scree with start input name
+    def screen_of_input_name(self):
+        def button_command():
+            #проверка ввода имени and start server
+            _name = self.entry.get()
+            if ' ' in _name or _name == '':
+                self.label.configure(text = "Имя не подходит")
+            else:
+                self.run_speak_with_server(_name)
+                button.destroy()
+
+        button = CTk.CTkButton(self, text = 'Подключиться', command = button_command)
+        button.grid(row=0, column=2, padx=20, pady=20)
+
+    # start work client server
     def run_speak_with_server(self, name):
+        self.label.configure(text = "CHAT")
+
         #client
         self.client = Client(name)
+        conn = self.client.connect()
+        if conn:
+            self.label.configure(text = "Успешное подключение")
+        else:
+            self.label.configure(text = "Это имя уже занято!")
+            self.screen_of_input_name()
+            return
 
         # add scroll char
-        self.chat_frame = Chat_frame(master=self, client = self.client, width=300, height=200)
+        self.chat_frame = Chat_frame(master=self, client = self.client, label = self.label, _func = self.run_speak_with_server, width=300, height=200)
         self.chat_frame.grid(row=0, column=0, padx=20, pady=20)
 
         #add button for send message
@@ -56,37 +77,39 @@ class Window_chat(CTk.CTk):
         self.button = CTk.CTkButton(self, text = 'Send', command = button_command)
         self.button.grid(row=0, column=2, padx=20, pady=20)
 
-
 class Chat_frame(CTk.CTkScrollableFrame):
-    def __init__(self, master, client, **kwargs):
+    def __init__(self, master, client, label, _func, **kwargs):
         super().__init__(master, **kwargs)
+        func = _func
+        self.label = label
         self.client = client
 
-        # add widgets onto the frame...
-        self.label = CTk.CTkLabel(self)
-        self.label.grid(row=0, column=0, padx=20)
         #thread for chat print
         t = threading.Thread(target=self.print_message)
         t.start()
 
-    def close(self):
-        self.destroy()
-
     def print_message(self):
         message = []
         lenght_chat = 10
-        while True:
+        quit = False
+        i = 1
+        while not quit:
             try:
-                _text = self.client.listen()
+                # recive message
+                quit, _text = self.client.listen()
+                if quit:
+                    self.label.configure(text = _text)
+                    break
+                #pozition in chat frame
                 _len = len(message)
                 message.append(CTk.CTkLabel(self, text = _text))
-                message[_len].grid(row=_len, column=0, padx=20)
+                message[_len].grid(row=i, column=0, padx=20)
                 if(_len == lenght_chat):
                     message.pop(0).destroy()
+                i+=1
             except Exception as e:
-                print('Err (print_message): ', e)
+                print('Работа закончена!', e)
                 break
-
 
 if __name__ == '__main__':
     app = Window_chat()
